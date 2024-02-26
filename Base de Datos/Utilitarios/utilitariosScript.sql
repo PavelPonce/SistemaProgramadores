1c76fad38508f4613881f6a9ceb030988f6f8c24
PavelPonce <136554909+PavelPonce@users.noreply.github.com>
2024-02-26 17:07:33 -0600

    Update utilitariosScript.sql

--- Base de Datos/Utilitarios/utilitariosScript.sql
+++ Base de Datos/Utilitarios/utilitariosScript.sql
@@ -4,7 +4,7 @@
 [Acces_FechaCreacion] [datetime] NOT NULL,
 [Acces_UsuarioModificacion] [int] NULL,
 [Acces_FechaModificacion] [datetime] NULL,
-[Acces_Estado] [bit] DEFAULT 1	
+[Acces_Estado] [bit] DEFAULT 1,
 CONSTRAINT FK_tablaPerteneciente_tbUsuarios_Acces_UsuarioCreacion FOREIGN KEY(Acces_UsuarioCreacion) REFERENCES Acces.tbUsuarios(Usuar_Id),
 CONSTRAINT FK_tablaPerteneciente_tbUsuarios_Acces_UsuarioModificacion FOREIGN KEY(Acces_UsuarioModificacion) REFERENCES Acces.tbUsuarios(Usuar_Id)
 
