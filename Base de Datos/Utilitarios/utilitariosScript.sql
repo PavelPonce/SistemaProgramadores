3160864f4ea1793f8951e9d3ffaf57824ad5e2df
PavelPonce <136554909+PavelPonce@users.noreply.github.com>
2024-02-26 16:17:08 -0600

    Creacion "utilitarios"
    Campos de auditoria a agregar (copy+paste) al final de cada tabla, al finalizar todo

---
+++ Base de Datos/Utilitarios/utilitariosScript.sql
@@ -0,0 +1,11 @@
+--Campos de auditoria
+
+[Acces_UsuarioCreacion] [int] NOT NULL,
+[Acces_FechaCreacion] [datetime] NOT NULL,
+[Acces_UsuarioModificacion] [int] NULL,
+[Acces_FechaModificacion] [datetime] NULL,
+[Acces_Estado] [bit] DEFAULT 1	
+CONSTRAINT FK_tablaPerteneciente_tbUsuarios_Acces_UsuarioCreacion FOREIGN KEY(Acces_UsuarioCreacion) REFERENCES Acces.tbUsuarios(Usuar_Id),
+CONSTRAINT FK_tablaPerteneciente_tbUsuarios_Acces_UsuarioModificacion FOREIGN KEY(Acces_UsuarioModificacion) REFERENCES Acces.tbUsuarios(Usuar_Id)
+
+--Campos de auditoria	
