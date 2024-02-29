775157f739e053180786d8c2a934f67bf72c0a61
Victor Espinoza <vespinozabenitez@gmail.com>
2024-02-29 09:41:57 -0600

    Login Home Controller
    Login Home Controller

--- SistemaAcademiaProgramadores/SistemaAcademiaProgramadores/Controllers/HomeController.cs
+++ SistemaAcademiaProgramadores/SistemaAcademiaProgramadores/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistemaAcademiaProgramadores.Models;
 
 namespace SistemaAcademiaProgramadores.Controllers
 {
     public class HomeController : Controller
     {
+        private dbAcademiaProgramadoresEntities2 db = new dbAcademiaProgramadoresEntities2();
+
         public ActionResult Index()
         {
             return View();
@@ -15,6 +18,23 @@ namespace SistemaAcademiaProgramadores.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string txtUsuario, string txtContra)
+        {
+            var usuario = db.SP_tbUsuarios_LOGIN(txtUsuario, txtContra).ToList();
+            if (usuario.Count > 0)
+            {
+                foreach (var item in usuario)
+                {
+                    Session["Usuario"] = item;
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
