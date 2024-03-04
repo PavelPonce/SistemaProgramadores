ba0ed1539bf0c0cfa1d080ecb592b124eba1196b
Victor Espinoza <vespinozabenitez@gmail.com>
2024-03-04 02:23:25 -0600

    Archivos para autenticacion y layout
    Esto va en el webconfig:
    <system.web>
        <compilation debug="true" targetFramework="4.7.2" />
        <httpRuntime targetFramework="4.7.2" />
    	  ESTO:<authentication mode="Forms">
    		  <forms loginUrl="Home/Login"></forms>
    	  </authentication>
    	  <roleManager defaultProvider="myRoleProvider" enabled="true">
    		  <providers>
    			  <clear/>
    			  <add name="myRoleProvider" type="SistemaAcademiaProgramadores.WebProvider"/>
    		  </providers>
    	  </roleManager>:HASTA AQUI
      </system.web>

---
+++ SistemaAcademiaProgramadores/SistemaAcademiaProgramadores/Attributes/CustomAuthorizeAttribute.cs
@@ -0,0 +1,80 @@
+﻿using SistemaAcademiaProgramadores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaAcademiaProgramadores.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public string ControllerName { get; set; }
+        static string ParsearControllerName(string input)
+        {
+            //Estos son casos especiales
+            if (input == "InstructoresPorCursoPorGeneracion")
+            {
+                return "Administracion de Cursos";
+            }
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            StringBuilder result = new StringBuilder(input[0].ToString());
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (char.IsUpper(input[i]) && char.IsLower(input[i - 1]))
+                {
+                    result.Append(' '); // Add a space before the uppercase letter
+                }
+
+                result.Append(input[i]);
+            }
+
+            return result.ToString();
+        }
+        private string GetRolesFromDatabase(string Panta_Descripcion)
+        {
+            dbAcademiaProgramadoresEntities2 db = new dbAcademiaProgramadoresEntities2();
+            var listaRolesPorPantalla = db.SP_PantallasPorRoles_SeleccionarRolesPorPantalla(Panta_Descripcion).ToList();
+
+            var str = "";
+            for (int i = 0; i < listaRolesPorPantalla.Count; i++)
+            {
+                if (i == listaRolesPorPantalla.Count - 1)
+                {
+                    str += listaRolesPorPantalla[i].Roles_Descripcion;
+                }
+                else
+                {
+                    str += $"{listaRolesPorPantalla[i].Roles_Descripcion}, ";
+                }
+            }
+            return str;
+        }
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            ControllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            string roles = GetRolesFromDatabase(ParsearControllerName(ControllerName));
+            Roles = roles;
+            base.OnAuthorization(filterContext);
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectResult("~/Home/Login");
+            }
+            else
+            {
+                filterContext.Result = new RedirectResult("~/Home/Index");
+            }
+        }
+    }
+}
---
+++ SistemaAcademiaProgramadores/SistemaAcademiaProgramadores/WebProvider.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using SistemaAcademiaProgramadores.Models;
+
+
+namespace SistemaAcademiaProgramadores
+{
+    public class WebProvider : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            using (var context = new dbAcademiaProgramadoresEntities2())
+            {
+                var result = (from usuario in context.tbUsuarios
+                              join rol in context.tbRoles on usuario.Roles_Id equals rol.Roles_Id
+                              where usuario.Usuar_Usuario == username
+                              select rol.Roles_Descripcion).ToArray();
+                return result;
+            }
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
