01c6a486afa3a11742c86187f1a9bb1128f3d4ff
PavelPonce <136554909+PavelPonce@users.noreply.github.com>
2024-03-04 08:17:13 -0600

    Create SPsLlenarEditar + DDLs.sql

---
+++ Base de Datos/SPsLlenarEditar + DDLs.sql
@@ -0,0 +1,44 @@
+CREATE PROCEDURE Mante.SP_Departamentos_LlenarEditar
+	@Depar_Id CHAR(2)
+AS
+BEGIN
+	SELECT *
+	FROM Mante.tbDepartamentos 
+	WHERE Depar_Id = @Depar_Id
+END
+GO
+CREATE PROCEDURE Mante.SP_Titulos_LlenarEditar
+	@Titul_Id INT
+AS
+BEGIN
+	SELECT *
+	FROM Mante.tbTitulos
+	WHERE Titul_Id = @Titul_Id
+END
+GO
+CREATE PROCEDURE Mante.SP_Municipios_LlenarEditar
+	@Munic_Id CHAR(4)
+AS
+BEGIN
+	SELECT mun.*, dep.Depar_Descripcion
+	FROM Mante.tbMunicipios AS mun INNER JOIN Mante.tbDepartamentos AS dep
+	ON mun.Depar_Id = dep.Depar_Id
+	WHERE Munic_Id = @Munic_Id
+END
+GO
+CREATE PROCEDURE Mante.SP_EstadosCiviles_LlenarEditar
+	@Estci_Id INT
+AS
+BEGIN
+	SELECT *
+	FROM Mante.tbEstadosCiviles
+END
+GO
+
+CREATE PROCEDURE Mante.SP_Titulos_DropDownList
+AS
+BEGIN
+	SELECT Titul_Tipo,
+			CASE Titul_Tipo WHEN 'DO' THEN 'Doctorado' WHEN 'PO' THEN 'Posgrado' WHEN 'PR' THEN 'Pregrado' ELSE 'Bachiller' END AS Titul_TipoDescripcion
+	FROM Mante.tbTitulos
+END
